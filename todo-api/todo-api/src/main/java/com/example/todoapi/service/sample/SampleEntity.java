6246f4ed738c92dae8b9611a1cc34d4e5f5f2877
junya-ohori <ohohutsuke.@gmail.com>
2024-05-24 21:21:56 +0900

    72.レスポンスボディから開始

---
+++ todo-api/todo-api/src/main/java/com/example/todoapi/service/sample/SampleEntity.java
@@ -0,0 +1,11 @@
+package com.example.todoapi.service.sample;
+
+import lombok.Value;
+
+import java.time.LocalDateTime;
+
+@Value
+public class SampleEntity {
+    String content;
+//    LocalDateTime timestamp;
+}
